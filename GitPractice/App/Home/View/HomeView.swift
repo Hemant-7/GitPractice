bb88ef8837ee4ee2c0a52a32a876266f85732eb9
Hemant Kumar <hmntkmr952807@gmail.com>
2022-12-16 15:27:19 +0530

    Create new branch development from main and add home

---
+++ GitPractice/App/Home/View/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  GitPractice
+//
+//  Created by Hemant on 16/12/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
